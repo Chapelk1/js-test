1f09847cd35654e15aed08db6bf8e6f5aba300e4
Chapelk1 <klepach154789@gmail.com>
2023-05-26 00:52:46 +0300

    +

---
+++ js/main.js
@@ -0,0 +1,71 @@
+
+
+
+
+// let value = prompt("random number");
+// value = Number(value);
+
+// console.log(value);
+// console.log(typeof value);
+
+
+// const min = 1;
+// const max = 6;
+// const resoult_main = Math.round(Math.random() * (max - min) + min);
+// console.log(resoult_main)
+
+// const resoult_second = Math.round(Math.random() * (max - min) + min);
+// console.log(resoult_second)
+
+// const resoult = resoult_main + resoult_second;
+// console.log(resoult);
+
+
+
+// let firstName = prompt('Введіть своє ім`я.');
+// firstName = firstName[0] + firstName.slice(1).toLowerCase();
+
+// let lastName = prompt('Введіть своє прізвище.');
+// lastName = lastName[0] + lastName.slice(1).toLowerCase();
+
+// const fullName = ` ${firstName}  ${lastName} `;
+
+// console.log(fullName)
+
+// const message = 'Curabitur ligula sapien';
+// const maxLength = 16;
+
+
+// function formatMessage(message, maxLength) {
+//   let result;
+//   // Change code below this line
+//    result = message.length <= maxLength ? message :
+//      `${message.slice(0, maxLength)}...`;
+//   /// Change code above this line
+//   return result;
+// }
+
+// console.log(formatMessage('Curabitur ligula sapien', 16))
+
+
+
+// const main = prompt('ДАЙ РЯДОК');
+// const sec = prompt('ДАЙ ЧИСЛО');
+
+// function jeka(string, length) {
+//     const substring = string.slice(0, length);
+
+//   return substring;
+// }
+
+// console.log(jeka(main, sec));
+
+// let a = 6; 
+// let b = false; 
+// let c = "12"; 
+// let d = 1; 
+// let result = a + d + c + b; 
+
+// result = a + Number(b) + Number(c) + d
+
+// console.log(result);
